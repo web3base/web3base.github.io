63fd1f1cb544d790817d2278077da89ff1af6f2e
test <example@github.com>
2024-06-11 13:55:45 +0000

    deploy: web3base/lady-mimi@25bb5bc41a8ef2a26a1a41d8a0db130c7d9be934

--- assets/index-DlXAuRAt.js
+++ assets/index-DlXAuRAt.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as k,t as P,a as g,o as Y,R as Dr,p as H,y as ae}from"./index-BV3JCrmS.js";import{aK as Ur}from"./index-1MVfF2Oa.js";/**
+import{n as fe,s as Ke,T as k,t as P,a as g,o as Y,R as Dr,p as H,y as ae}from"./index-CxlLQU9M.js";import{aK as Ur}from"./index-Sg65Aide.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
